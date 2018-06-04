9c21a04eeffdd75f7c6edd9e011ae22f1d5852e8
Manish Rayamajhi <37249602+manishry@users.noreply.github.com>
2018-06-04 13:46:42 +0545

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,488 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>prject</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  
+  
+   <link rel="stylesheet" type="text/css" href="style.css">
+   
+<style type="text/css">
+ .zero-margin{
+    margin: 0px;
+ }
+ .zero-padding{
+  padding: 0px;
+ }
+    
+  .title-img{
+    margin: 0px;
+    padding-bottom: 1px;
+    padding-top: 10px;
+    font-weight: 800;
+    color: rgb(56, 65, 74);
+    font-family: "Proxima Nova", sans-serif;
+    font-size: 18px;
+    text-align: center;
+    line-height: 1.16667;
+  }  
+  .side-img{
+    width: 172.175px;
+    height: 120.94px;
+    border-radius: 40px;
+  }
+  .small-img{
+    width: 142.175px;
+    height: 120.94px;
+    border-radius: 40px;
+
+  }
+  .style-content{
+    width: 171.175px;
+
+  }
+  .image-gap{
+    width:200px;
+  }
+  
+    
+</style>
+  
+
+
+</head>
+<body>
+  <div class="row top-menu">
+    <div class="container">
+    
+      <div class="header-top-content">
+              <a href="">Home | </a>
+              <a href="">Aims |</a>
+              <a href="">Our Team |</a>
+              <a href=""> About |</a>
+              <a href="">UsContact |</a>
+              <a href="">UsNewsletter |</a>
+              <a href="">Videos |</a>
+              <a href="">Forums |</a>
+            </div>
+          
+  </div>
+</div>
+
+  
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-primary" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+ <!--main page image slider-->
+<div class="container">
+ <div class="row">
+  <div class="col-xs-12">
+  <div class="col-xs-3">
+    <ul class="list-group">
+      <li class="list-group-item"><h2><b>menu bar</b></h2></li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      
+       
+    </ul>
+  </div>
+<div class="col-xs-9 ">
+  <div class="list-group"><center class="list-group-item list-group-item-success">Inkmonk.com - Your One Stop Solution For High Quality Online Printing</center></div>
+<div class="row">
+
+  <div class="col-md-4"><center><span class="glyphicon">&#xe108;</span>On time delivery</center></div>
+  <div class="col-md-4"><center><span class="glyphicon">&#8377;</span>Always at best price</center></div>
+
+ <div class="col-md-4" style="padding-bottom: 10px;"><center><span class="glyphicon">&#xe084;</span>Free design check</center></div>
+
+</div>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+    <li data-target="#myCarousel" data-slide-to="4"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="image/slider1.png" width="100%" height="20%">
+    </div>
+
+    <div class="item">
+      <img src="image/slider2.png" width="100%" height="20%">
+    </div>
+
+    <div class="item">
+      <img src="image/slider3.png" width="100%" height="20%">
+    </div>
+
+    <div class="item">
+      <img src="image/slider4.png" width="100%" height="20%">
+    </div>
+
+    <div class="item">
+      <img src="image/slider5.png" width="100%" height="20%">
+    </div>
+
+   
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- end of slider -->
+
+<div class="row" style="margin-top: 10px;">
+  <div class="col-md-12 zero-margin zero-padding" >
+ <div class="col-md-6">
+    <a href="#"><img src="image/img1.png" width="100%"></a>
+    
+  </div>
+
+  <div class="col-md-6">
+   <a href="#"> <img src="image/img2.png" width="100%"></a>
+    
+  </div>
+</div>
+<div class="col-md-12 zero-margin zero-padding" style="margin-top: 10px;">
+  
+   <div class="col-md-6">
+    
+    <a href="#"><img src="image/img3.png"  width="100%" ></a>
+  </div>
+
+  <div class="col-md-6">
+   
+    <a href="#"><img src="image/img4.png"  width="100%" ></a>
+  </div>
+   </div>
+ </div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 10px;">
+    <div class="col-md-2">  <a href="#"><img src="image/shop-img4.jpg"  width="100%" ></a></div>
+     <div class="col-md-2">  <a href="#"> <img src="image/shop-img1.png" width="100%"></a></div>
+    
+    <div class="col-md-2">  <a href="#"><img src="image/shop-img2.png"  width="100%"></a></div>
+     <div class="col-md-2">  <a href="#"><img src="image/shop-img5.png" width="100%"></a></div>
+    
+    <div class="col-md-2">  <a href="#"><img src="image/shop-img3.png"  width="100%"></a></div>
+    <div class="col-md-2">  <a href="#"><img src="image/shop-img6.png" width="100%"></a></div>
+   
+  </div>
+</div>
+
+
+<div class="container ">
+  <div class="row">
+    
+      <div class="title-img">
+          <center><h3>Get your complete business printing done at best quality</h3></center>
+      </div>
+    <footer><center>Get customized printing done on 30+ product categories at lowest prices.</center></footer>
+   
+  </div>
+</div>
+<div class="container">
+  <div class="row"> 
+    <div><h2><center>4 simple steps to print your products online</center></h2></div>
+    <div class="col-md-3"><img src="image/choosespecial1.png"  width="100%" ><p><h4>Choose your Spec</h4></p>
+      <footer>Select your customization options such as type, size of the product.</footer>
+   </div>
+    <div class="col-md-3"><img src="image/addToNetwork.png"  width="100%" ><p><h4>Add your Artwork</h4></p>
+     <p> Design online or upload your print file</p>
+    </div>
+    <div class="col-md-3"><img src="image/pickaprintshop.png"  width="100%" ><p><h4>Pick a Print Shop</h4></p>
+     <p>hoose the seller of your interest and pay online through our secure payment gateway</p>
+    </div>
+    <div class="col-md-3"><img src="image/done.png"  width="100%" ><p><h4>You are Done</h4></p>
+    <p>Inkmonk will review your design and deliver the ordered products at your doorstep</p>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <h1><center>Do you want to cut your business marketing expenses by 60%?</center></h1>
+    <p><center>flyer and pamphlets at the lowest cost in India</center></p>
+    <p style="font-color: red"><center>Know more</center></p>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 10px;">
+    <div class="col-md-5 side-img">
+      <img src="image/office.png"  width="100%" >
+     </div>
+     
+       <div class="col-md-1 small-img"><a href="#"><img src="image/img5.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img6.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img7.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img8.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img9.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img10.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img11.png"  width="100%"></a></div>
+     
+     
+      
+  </div>
+</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 10px;">
+    <div class="col-md-5 side-img">
+      <img src="image/gifting.png"  width="100%" >
+     </div>
+       <div class="col-md-1 small-img"><a href="#"><img src="image/img12.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img13.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img14.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img15.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img16.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img17.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img18.png"  width="100%"></a></div>
+      
+  
+     
+      
+  </div>
+</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 10px;">
+   <div class="col-md-5 side-img">
+      <img src="image/marketing.png"  width="100%" >
+     </div>
+       <div class="col-md-1 small-img"><a href="#"><img src="image/img19.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img20.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img21.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img22.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img23.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img24.png"  width="100%"></a></div>
+      <div class="col-md-1 small-img"><a href="#"><img src="image/img9.png"  width="100%"></a></div>
+     
+      
+  </div>
+</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 10px;">
+    <div class="col-md-6"><a href="#"><img src="image/customerbag.png" width="100%"></a>
+      <p><center>Let your customer carry your business</center></p>
+      <p><center><a href="#">Know more about paper bag</a></center></p>
+    </div>
+    <div class="col-md-6"><a href="#"><img src="image/carrybag.png" width="100%"></a>
+      <p><center>Premium bag for your minimal budget</center></p>
+      <p><center><a href="#">Know more about non-woven bag</a></center></p>
+    </div>
+    <hr>
+  </div>
+</div>
+<div class="container" >
+  <div><h3><center>Starting your own business?</center></h3>
+    <p><center>Save 60% on printing cost for your venture by choosing <a href="#">Business stationery combo</a> and  
+    <a href="#"> Startup Kit.</a></center></p>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 10px;">
+    <div class="col-md-6"><a href="#"><img src="image/stationery.png" width="100%"></a>
+     <h3><center> Stationery Combo</center></h3>
+     <h4><center>Letterheads, Envelopes, Notepad and Scribble sheets</center></h4>
+    <a href="#"><h5><center>Morre Details</center></h5></a>
+    </div>
+    <div class="col-md-6"><a href="#"><img src="image/startup.png" width="100%"></a>
+      <h3><center> Startup combo </center></h3>
+     <h4><center>T-Shirts, Mugs, Notebooks and Pens</center></h4>
+     <a href="#"><h5><center>Morre Details</center></h5></a>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 30px;">
+    <div class="col-md-4 style-content" style="margin-top: 10px;"><h3><b>Some of our great customers</b></h3></div>
+     
+        <div class="col-md-2"><img src="image/Unilever.png"  width="100%" ></div>
+       
+          <div class="col-md-2"><img src="image/Jugnoo.png" width="100%"></div>
+         
+        
+          <div class="col-md-2"><img src="image/IIT_Chennai.png" width="100%"></div>
+  
+          <div class="col-md-2 image-gap"><img src="image/Gojek.png" width="100%"></div>
+    
+    </div>
+      <div class="row" style="margin-top: 10px;">
+    <div class="col-md-4 style-content"></div>
+     
+        <div class="col-md-2 "><img src="image/Exotel.png" width="100%"></div>
+     
+          <div class="col-md-2"><img src="image/Droom.png" width="100%"></div>
+          
+          <div class="col-md-2"><img src="image/Codechef.png" width="100%"></div>
+         
+          <div class="col-md-2"><img src="image/Paypal.png" width="100%"></div>
+    
+    </div>
+
+ 
+</div>
+<!--
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 news-links">
+          <div>
+            <img src="image/img8.jpeg" width="100%">
+          </div>
+         
+         <div class="row" style="margin-top: 20px">
+           <div class="col-md-6" style="padding-bottom: 20px">
+             <img src="image/img4.jpeg" width="100%">
+           </div>
+           <div class="col-md-6" style="padding-bottom: 20px">
+             <img src="image/img2.jpg" width="100%">
+           </div>
+         </div>
+         </div>
+        <div class="col-md-4">
+          <div class="right-post" style="margin-top: 50px">
+             <img src="image/img7.jpeg" width="100%">
+          </div>
+         
+         <div class="right-post" style="margin-top: 40px">
+             <img src="image/img6.jpeg" width="100%">
+          </div>
+
+          <div class="right-post" style="margin-top: 40px">
+             <img src="image/img5.jpeg" width="100%">
+          </div>
+
+          
+
+          
+
+        </div>
+      </div>
+      </div>
+    </div>
+       -->   
+<!--Footer-->
+<footer class="page-footer center-on-small-only stylish-color-dark">
+
+    <!--Footer Links-->
+    <div class="container">
+        <div class="row">
+
+            
+            
+            
+            <!--/.Third column-->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!--Fourth column-->
+            <div class="col-md-4 mx-auto">
+                <h2 class="title mb-4 mt-3 font-bold ">Interact</h2>
+                <ul>
+                    <li><a href="#!">Like Us On Facebook</a></li>
+                    <li><a href="#!">Follow Us On Twitter</a></li>
+                    <li><a href="#!">Join Us On Google+</a></li>
+                    <li><a href="#!"> Subscribe RSS Feeds</a></li>
+                     <li><a href="#!"> Contact us</a></li>
+                    
+                </ul>
+            </div>
+            <!--/.Fourth column-->
+        </div>
+         
+<hr>
+<!--Social buttons-->
+    <div class="social-section pull-left">
+        <ul>
+
+            <li><a class="btn-floating btn-sm btn-fb"><i class="fa fa-facebook"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-tw"><i class="fa fa-twitter"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-gplus"><i class="fa fa-google-plus"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-li"><i class="fa fa-linkedin"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-dribbble"><i class="fa fa-dribbble"> </i></a></li>
+
+        </ul>
+    </div>
+    <!--/.Social buttons-->
+
+    <!--Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+            © 2017 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+
+        </div>
+    </div>
+    <!--/.Copyright-->
+
+    </div>
+    <!--/.Footer Links-->
+
+    
+
+</footer>
+<!--/.Footer-->
+        
+
+
+       <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+       <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        
+</body>
+</html>
+ 
